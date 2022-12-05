1108252a9c27a49f1a44afedd5cc10b0c137efc6
Noah Jiang <nujiang@ucsc.edu>
2022-12-04 21:09:35 -0800

    add walls and player

---
+++ CMPM121_Mystery_Scene/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController cc;
+
+    public float moveSpeed;
+
+    public float jumpSpeed;
+
+    private float horizontalMove, verticalMove;
+
+    private Vector3 dir;
+
+    private void Start(){
+        cc= GetComponent<CharacterController>();
+    }
+
+    private void Update(){
+        horizontalMove = Input.GetAxis ("Horizontal") * moveSpeed;
+        verticalMove = Input.GetAxis ("Vertical")* moveSpeed;
+
+        dir = transform.forward * verticalMove + transform.right * horizontalMove;
+        cc.Move(dir*Time.deltaTime);
+    }
+
+}
