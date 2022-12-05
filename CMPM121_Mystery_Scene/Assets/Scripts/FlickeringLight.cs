4313a59b558b824b983c9eea69464f9ef84831d6
DiADIAAAA <75306414+DiADIAAAA@users.noreply.github.com>
2022-12-05 03:46:37 -0800

    add flickering light and soft shadowing

---
+++ CMPM121_Mystery_Scene/Assets/Scripts/FlickeringLight.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=DNMdu3kylec
+    public bool isFlickering = false;
+    public float timeDelay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFlickering == false){
+            StartCoroutine(FlickeringControl());
+        }
+    }
+    IEnumerator FlickeringControl(){
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(0.01f, 0.1f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+}
