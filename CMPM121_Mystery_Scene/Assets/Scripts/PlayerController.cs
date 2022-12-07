6b949df212539b87786a542289d93605f5660368
TIANGE WEI <twei14@ucsc.edu>
2022-12-07 10:26:19 -0800

    Fixed camera, collision box, add gravity

--- CMPM121_Mystery_Scene/Assets/Scripts/PlayerController.cs
+++ CMPM121_Mystery_Scene/Assets/Scripts/PlayerController.cs
@@ -14,16 +14,35 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 dir;
 
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _groundDistance = 0.4f;
+    [SerializeField] private LayerMask _groundMask;
+    
+    private Vector3 _velocity;
+    private bool _isGrounded;
+
     private void Start(){
         cc= GetComponent<CharacterController>();
     }
 
-    private void Update(){
+    private void Update()
+    {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+        
         horizontalMove = Input.GetAxis ("Horizontal") * moveSpeed;
         verticalMove = Input.GetAxis ("Vertical")* moveSpeed;
 
         dir = transform.forward * verticalMove + transform.right * horizontalMove;
         cc.Move(dir*Time.deltaTime);
+
+        _velocity.y += _gravity * Time.deltaTime;
+        cc.Move(_velocity * Time.deltaTime);
+
     }
 
 }
